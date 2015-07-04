926fa5fb7e3a49d9f883f945441dab150cfc434b
JG Miller <jg.millirem@gmail.com>
2015-07-03 21:33:56 -0700

    * Always show main window maximized upon startup.

--- src/zyzzyva/zyzzyva.cpp
+++ src/zyzzyva/zyzzyva.cpp
@@ -52,6 +52,7 @@ int main(int argc, char** argv)
     window->tryConnectToDatabases();
 
     window->show();
+    window->setWindowState(Qt::WindowMaximized);
     splash->finish(window);
     delete splash;
 
