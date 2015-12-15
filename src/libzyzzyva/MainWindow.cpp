f6fba9a1a5cb8578d737bf96f9e716b02fb5f301
JG Miller <jg.millirem@gmail.com>
2015-12-14 19:34:48 -0800

    -Disable QAssistantClient help finally.

--- src/libzyzzyva/MainWindow.cpp
+++ src/libzyzzyva/MainWindow.cpp
@@ -329,7 +329,9 @@ MainWindow::MainWindow(QWidget* parent, QSplashScreen* splash, Qt::WindowFlags f
     QAction* helpAction = new QAction("&Help", this);
     Q_CHECK_PTR(helpAction);
     helpAction->setIcon(QIcon(":/help-icon"));
-    connect(helpAction, SIGNAL(triggered()), SLOT(displayHelp()));
+    //connect(helpAction, SIGNAL(triggered()), SLOT(displayHelp()));
+    // Start using "Welcome" dialog solely for providing help!
+    connect(helpAction, SIGNAL(triggered()), SLOT(newIntroForm()));
     helpMenu->addAction(helpAction);
 
     // About
@@ -384,7 +386,9 @@ MainWindow::MainWindow(QWidget* parent, QSplashScreen* splash, Qt::WindowFlags f
     toolbar->addAction(toolbarEditPrefsAction);
     QAction* toolbarHelpAction = new QAction("Help", this);
     copyQActionPartial(helpAction, toolbarHelpAction);
-    connect(toolbarHelpAction, SIGNAL(triggered()), SLOT(displayHelp()));
+    //connect(toolbarHelpAction, SIGNAL(triggered()), SLOT(displayHelp()));
+    // Start using "Welcome" dialog solely for providing help!
+    connect(toolbarHelpAction, SIGNAL(triggered()), SLOT(newIntroForm()));
     toolbar->addAction(toolbarHelpAction);
     addToolBar(toolbar);
 
