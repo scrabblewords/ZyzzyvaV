63f59fae7739db9ce18e06d433d8ed7dae0237d6
JG Miller <jg.millirem@gmail.com>
2016-02-05 22:52:33 -0800

    - Updated copyright year to 2016

--- src/libzyzzyva/ZPushButton.h
+++ src/libzyzzyva/ZPushButton.h
@@ -4,7 +4,7 @@
 // A class derived from QPushButton.  This class allows buttons to be clicked
 // by pressing Enter as well as the space bar.
 //
-// Copyright 2015 Twilight Century Computing.
+// Copyright 2016 Twilight Century Computing.
 // Copyright 2006-2012 North American SCRABBLE Players Association.
 //
 // This file is part of Zyzzyva.
