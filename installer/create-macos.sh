6733f68a6f52899c5b127b56552f8cc1121d64f0
Anand Buddhdev <anandb@ripe.net>
2019-05-10 15:44:27 +0700

    Bundle QtAssistant more smartly
    This change bundles just the QtAssistant binary, and changes its
    library paths to point to frameworks already in the Zyzzyva app.
    This reduces the size of the Mac app even further.

--- installer/create-macos.sh
+++ installer/create-macos.sh
@@ -11,8 +11,10 @@ make
 cd bin
 mv libzyzzyva5*dylib Zyzzyva.app/Contents/MacOS/
 install_name_tool -change libzyzzyva5.1.dylib @executable_path/libzyzzyva5.1.dylib Zyzzyva.app/Contents/MacOS/Zyzzyva
-cp -R ../data $QTDIR/libexec/Assistant.app Zyzzyva.app/Contents/MacOS
-macdeployqt Zyzzyva.app/Contents/MacOS/Assistant.app
-macdeployqt Zyzzyva.app
+cp $QTDIR/libexec/Assistant.app/Contents/MacOS/Assistant Zyzzyva.app/Contents/MacOS
+install_name_tool -add_rpath '@loader_path/../Frameworks' Zyzzyva.app/Contents/MacOS/Assistant
+macdeployqt Zyzzyva.app -executable=Zyzzyva.app/Contents/MacOS/Assistant
+cp -R ../data Zyzzyva.app/Contents/MacOS
+rm -f Collins-Zyzzyva-5.1.0.zip
 zip -r Collins-Zyzzyva-5.1.0.zip Zyzzyva.app
 zip -j Collins-Zyzzyva-5.1.0.zip ../installer/MacOS-Installation-Instructions.txt
