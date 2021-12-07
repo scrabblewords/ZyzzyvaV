82f18669c24502c9d0f3b82ab77cc1e7cec5e341
Anand Buddhdev <anandb@ripe.net>
2021-12-07 11:38:04 +0100

    Rewrite the macos build script
    Recursive qmake followed by "make clean" ensures that we start with
    a clean source tree. The build stage now uses more CPUs for faster
    builds. Finally, we create a .DMG instead of a .ZIP file. This is
    the usual way to distribute mac apps.

--- installer/create-macos.sh
+++ installer/create-macos.sh
@@ -13,15 +13,22 @@ export PKG_CONFIG_PATH=$QTDIR/lib/pkgconfig
 
 VERSION=$(tr -d '\n' < VERSION)
 
-qmake
-make
+$QTDIR/bin/qmake -recursive
+rm -rf bin/*
+make clean
+
+make -j $(sysctl -a | awk '{ if ($1 == "machdep.cpu.core_count:") print $2 }')
+cp -R data bin/Zyzzyva.app/Contents/MacOS
 cd bin
 mv libzyzzyva5*dylib Zyzzyva.app/Contents/MacOS/
 install_name_tool -change libzyzzyva5.1.dylib @executable_path/libzyzzyva5.1.dylib Zyzzyva.app/Contents/MacOS/Zyzzyva
 cp $QTDIR/libexec/Assistant.app/Contents/MacOS/Assistant Zyzzyva.app/Contents/MacOS
 install_name_tool -add_rpath '@loader_path/../Frameworks' Zyzzyva.app/Contents/MacOS/Assistant
-macdeployqt Zyzzyva.app -executable=Zyzzyva.app/Contents/MacOS/Assistant
-cp -R ../data Zyzzyva.app/Contents/MacOS
-rm -f Collins-Zyzzyva-$VERSION.zip
-zip -r Collins-Zyzzyva-$VERSION.zip Zyzzyva.app
-zip -j Collins-Zyzzyva-$VERSION.zip ../installer/MacOS-Installation-Instructions.txt
+$QTDIR/bin/macdeployqt Zyzzyva.app -executable=Zyzzyva.app/Contents/MacOS/Assistant -dmg
+hdiutil convert Zyzzyva.dmg -format UDRW -o ZyzzyvaRW.dmg
+rm Zyzzyva.dmg
+hdiutil attach ZyzzyvaRW.dmg
+cp ../installer/MacOS-Installation-Instructions.txt /Volumes/Zyzzyva
+hdiutil detach /Volumes/Zyzzyva
+hdiutil convert ZyzzyvaRW.dmg -format UDZO -o Collins-Zyzzyva-$VERSION.dmg
+rm ZyzzyvaRW.dmg
